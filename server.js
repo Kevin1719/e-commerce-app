38de780a54c3250a87a60b9732b0b54cd2b37e4e
Kevin1719 <93263952+Kevin1719@users.noreply.github.com>
2022-07-08 20:13:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + './dist/e-commerce-app'));
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname +
-        './dist/e-commerce-app'));
+app.use(express.static('./dist/e-commerce-app/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/e-commerce-app/' })
 });
 app.listen(process.env.PORT || 8080);
